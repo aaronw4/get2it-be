21c2ef361b22fc008bc655792db90d2d956a948a
Aaron Williams <aaronrw4@yahoo.com>
2020-06-24 22:11:27 -0700

    created delete catTask function

--- categories/cat-task-model.js
+++ categories/cat-task-model.js
@@ -4,7 +4,8 @@ module.exports = {
     findById,
     assignCategory,
     update,
-    findTasks
+    findTasks,
+    remove
 };
 
 // (id) below is the task id
@@ -32,5 +33,11 @@ function update(combo, id) {
       .join('categories', 'categories.id', 'tc.category_id')
       .select('t.name', 't.status', 't.date', 't.start_time', 't.end_time', 't.task_icon', 't.timeLeft', 't.initialNotify', 't.notifyOn', 't.id', 't.user_id')
       .where({category_id: id})
-};
+}
+
+function remove(id) {
+    return db('task-categories')
+        .where({task_id: id})
+        .delete()
+}
   
